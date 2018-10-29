1faf9444e3b5ad9cf83797458a112f09df46726f
Arun Bansal <bansal.arun27@gmail.com>
2018-10-29 17:26:44 +0530

    database

--- db/invoice_test.sql
+++ db/invoice_test.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 26, 2018 at 07:45 PM
+-- Generation Time: Oct 29, 2018 at 12:51 PM
 -- Server version: 10.1.36-MariaDB
 -- PHP Version: 7.2.10
 
@@ -25,6 +25,29 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `customer`
+--
+
+CREATE TABLE `customer` (
+  `id` int(11) NOT NULL,
+  `first_name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `last_name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `customer`
+--
+
+INSERT INTO `customer` (`id`, `first_name`, `last_name`, `email`) VALUES
+(1, 'Arun', 'Bansal', 'bansal.arun27@gmail.com'),
+(2, 'John', 'rock', 'john.rock@gmail.com'),
+(3, 'Migual', 'Mendoza', 'mingual@gmail.com'),
+(4, 'Michal', 'Aul', 'michalaul@gmail.com');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `invoice`
 --
 
@@ -40,8 +63,10 @@ CREATE TABLE `invoice` (
 --
 
 INSERT INTO `invoice` (`id`, `invoice_date`, `invoice_number`, `customer_id`) VALUES
-(1, '2018-10-04', 434, 5),
-(2, '2018-10-04', 435, 5);
+(1, '2018-10-04', 434, 2),
+(2, '2018-10-03', 21, 1),
+(3, '2018-10-04', 435, 3),
+(4, '2018-10-04', 435, 4);
 
 -- --------------------------------------------------------
 
@@ -63,12 +88,20 @@ CREATE TABLE `invoice_detail` (
 --
 
 INSERT INTO `invoice_detail` (`id`, `invoice_id`, `description`, `amount`, `vat_amount`, `total_amount`) VALUES
-(1, 1, 'jfsk df', 23, 12, 35),
-(2, 2, 'arun bansal', 23, 12, 35);
+(1, 1, 'Testing', 23, 12, 35),
+(2, 2, 'arun bansal1', 40, 12, 52),
+(3, 3, 'jfsk df', 55, 45, 100),
+(4, 4, 'jfsk df', 55.6, 45.6, 101.2);
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `customer`
+--
+ALTER TABLE `customer`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `invoice`
@@ -88,16 +121,22 @@ ALTER TABLE `invoice_detail`
 --
 
 --
+-- AUTO_INCREMENT for table `customer`
+--
+ALTER TABLE `customer`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
 -- AUTO_INCREMENT for table `invoice`
 --
 ALTER TABLE `invoice`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `invoice_detail`
 --
 ALTER TABLE `invoice_detail`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- src/AppBundle/Controller/InvoiceController.php
+++ src/AppBundle/Controller/InvoiceController.php
@@ -2,30 +2,108 @@
 
 namespace AppBundle\Controller;
 use AppBundle\Entity\Invoice;
+use AppBundle\Entity\Customer;
 use AppBundle\Entity\Invoice_detail;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class InvoiceController extends Controller
 {
     /**
-     * @Route("/invoice", name="invoice_page")
+     * @Route("/invoice/", name="invoice_page")
      */
     public function indexAction()
     {
-        return $this->render('invoice/index.html.twig');
+       $em=$this->getDoctrine()->getManager();
+        $query= $em->createQuery('
+          SELECT i.id as id,i.invoiceNumber,i.invoiceDate,i.customerId ,d.description
+          ,d.amount,d.vatAmount,d.totalAmount,c.email,c.firstName,c.lastName
+          FROM
+          AppBundle\Entity\Invoice i
+          LEFT JOIN AppBundle\Entity\Customer c WITH i.customerId=c.id
+          LEFT JOIN AppBundle\Entity\Invoice_detail d WITH i.id=d.invoiceId
+          ');
+          $result=$query->execute();    
+        return $this->render('invoice/index.html.twig',array('invoice'=>$result));
     }
     /**
-     * @Route("/invoice/form", name="form_page")
+     * @Route("/invoice/form/{id}", name="form_page_edit")
+     */
+    public function editAction($id,Request $request)
+    {
+             $em=$this->getDoctrine()->getManager();
+          $query= $em->createQuery("
+              SELECT c.email as email,c.id as id FROM AppBundle\Entity\Customer c
+          ");
+          $result=$query->execute();
+          foreach ($result as $value) {
+            $val[$value["email"]]=$value["id"];
+          }
+          $em->flush();
+       $em=$this->getDoctrine()->getManager();
+        $query= $em->createQuery("
+          SELECT i.id as id,i.invoiceNumber,i.customerId ,d.description
+          ,d.amount,d.vatAmount,d.totalAmount , d.id as did
+          FROM
+          AppBundle\Entity\Invoice i
+          LEFT JOIN AppBundle\Entity\Invoice_detail d WITH i.id=d.invoiceId
+          where i.id='$id'");
+          $result=$query->execute();
+        $invoice_detail = new Invoice_detail();
+        $invoice = new Invoice();
+        $inv[]=$invoice;
+        $inv[]=$invoice_detail;
+        
+        $form = $this->createFormBuilder($inv)
+        
+            ->add('invoice_number', TextType::class,array('data'=>$result["0"]["invoiceNumber"]))
+            ->add('description', TextType::class,array('data'=>$result["0"]["description"]))
+             ->add('cutomer', ChoiceType::class,array('choices'=>array($val),'data'=>$result["0"]["customerId"]))
+            ->add('amount', TextType::class,array('data'=>$result["0"]["amount"]))
+            ->add('vat_amount', TextType::class,array('data'=>$result["0"]["vatAmount"]))
+            ->add('save', SubmitType::class, array('label' => 'Submit'))
+            ->getForm();
+
+        $form->handleRequest($request);
+        
+        if ($form->isSubmitted() && $form->isValid()) {
+          $em=$this->getDoctrine()->getManager();
+          $invoice=$em->getRepository('AppBundle:Invoice')->find($id);
+           $invoice_number = $form["invoice_number"]->getData();
+         
+           $invoice->setInvoiceNumber($invoice_number);
+           
+           $invoice->setCustomerId($form["cutomer"]->getData());
+          
+           $em->flush();
+                     $em=$this->getDoctrine()->getManager();
+           $invoice_detail=$em->getRepository('AppBundle:Invoice_detail')->find(array(
+            "id"=>$result["0"]["did"]));
+            $invoice_detail->setDescription($form["description"]->getData());
+           $invoice_detail->setAmount($form["amount"]->getData());
+           $invoice_detail->setVatAmount($form["vat_amount"]->getData());
+           $invoice_detail->setTotalAmount($form["vat_amount"]->getData()+$form["amount"]->getData());
+           $em->flush();
+        }
+
+        return $this->render('invoice/form.html.twig', array(
+          'invoice'=>$invoice,
+            'form' => $form->createView()
+        ));
+    }
+     /**
+     * @Route("/invoice/form/", name="form_page")
      */
     public function formAction(Request $request)
     {
         $invoice = new Invoice();
         $invoice_detail = new Invoice_detail();
+       
         $inv[]=$invoice;
         $inv[]=$invoice_detail;
         $form = $this->createFormBuilder($inv)
